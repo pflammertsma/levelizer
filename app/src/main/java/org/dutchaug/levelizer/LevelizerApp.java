cbda14c163e0782d037933ab457223b1b0b74138
Paul Lammertsma <pflammertsma@gmail.com>
2016-07-08 14:51:48 +0300

    added EasyPreferences

---
+++ app/src/main/java/org/dutchaug/levelizer/LevelizerApp.java
@@ -0,0 +1,24 @@
+package org.dutchaug.levelizer;
+
+import android.app.Application;
+import android.content.ContextWrapper;
+
+import com.pixplicity.easyprefs.library.Prefs;
+
+
+public class LevelizerApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize the Prefs class
+        new Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
+    }
+
+}
