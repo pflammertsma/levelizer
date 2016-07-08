353b725933858e9631c84eed1c80bd6705c6767f
Frank Bouwens <frankkie12345@gmail.com>
2016-07-08 14:59:23 +0200

    fixed onboarding activity

---
+++ app/src/main/java/org/dutchaug/levelizer/OnboardingActivity.java
@@ -0,0 +1,29 @@
+package org.dutchaug.levelizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        Button enableButton = (Button) findViewById(R.id.onboarding_enable_btn);
+        enableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+                //startActivityForResult(i, 1337);
+                startActivity(i);
+            }
+        });
+
+        Button whitelisting = (Button) findViewById(R.id.onboarding_whitelist_btn);
+
+    }
+}
