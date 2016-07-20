4fd2dcc888c18fc1aa5db85811ac60e9c4adeefa
Paul Lammertsma <paul@pixplicity.com>
2016-07-20 15:16:30 +0200

    fixed compatibility for vector drawable

--- app/src/main/java/org/dutchaug/levelizer/LevelizerApp.java
+++ app/src/main/java/org/dutchaug/levelizer/LevelizerApp.java
@@ -2,6 +2,7 @@ package org.dutchaug.levelizer;
 
 import android.app.Application;
 import android.content.ContextWrapper;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.crashlytics.android.Crashlytics;
 import com.pixplicity.easyprefs.library.Prefs;
@@ -10,6 +11,10 @@ import io.fabric.sdk.android.Fabric;
 
 
 public class LevelizerApp extends Application {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     public void onCreate() {
